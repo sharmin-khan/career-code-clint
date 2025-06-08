49b8a5fceb41997840198e265f219932edf36748
sharmin-khan <sharminkhancse@gmail.com>
2025-06-08 22:53:48 +0600

    Added RootLayout, routing, and common components.

---
+++ src/Router/router.jsx
@@ -0,0 +1,22 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+import RootLayout from "../Layouts/RootLayout";
+import Home from "../Pages/Home/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayout,
+    children:[
+        {
+            index:true,
+            Component: Home
+        }
+    ]
+
+  },
+]);
+
+export default router;
